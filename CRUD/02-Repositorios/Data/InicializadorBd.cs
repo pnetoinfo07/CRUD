eb1d75f9e3672b9665fd6ca4acc97d0ade0d13b6
pnetoinfo07 <31972103+netiim@users.noreply.github.com>
2024-08-23 16:07:28 -0300

    Inicializando o banco

---
+++ CRUD/02-Repositorios/Data/InicializadorBd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD._02_Repositorios.Data;
+
+public static class InicializadorBd
+{
+    private const string ConnectionString = "Data Source=CRUD.db";
+
+    public static void Inicializar()
+    {
+        using (var connection = new SQLiteConnection(ConnectionString))
+        {
+            connection.Open();
+            string commandoSQL = @"   
+                 CREATE TABLE IF NOT EXISTS Times(
+                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 Nome TEXT NOT NULL,
+                 AnoCriacao INTEGER NOT NULL
+                );";
+            using (var command = new SQLiteCommand(commandoSQL, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
