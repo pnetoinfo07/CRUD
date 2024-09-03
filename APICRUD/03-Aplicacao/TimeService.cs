a7467641af9631791229d21876d8b9f4af60ff2d
pnetoinfo07 <31972103+netiim@users.noreply.github.com>
2024-09-03 17:01:30 -0300

    versão final

--- APICRUD/03-Aplicacao/TimeService.cs
+++ APICRUD/03-Aplicacao/TimeService.cs
@@ -31,7 +31,6 @@ namespace CRUD.Aplicacao
         {
             return repository.Listar();
         }
-
         public Time BuscarTimePorId(int id)
         {
            return repository.BuscarPorId(id);
